a70ab5eab39e36e652ece15349a9c9c723d2a2b9
Brian <riceb53@gmail.com>
2019-08-08 19:27:41 -0500

    add happy/sad path

--- src/views/BooksEdit.vue
+++ src/views/BooksEdit.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div v-for="error in errors">
+      {{error}}
+    </div>
     <form v-on:submit.prevent="submit()">
-      <p>Name: <input type="text" v-model="bookName"></p>
-      <p>Pages: <input type="text" v-model="bookPages"></p>
+      <p>Name: <input type="text" v-model="book.name"></p>
+      <p>Pages: <input type="text" v-model="book.pages"></p>
       <input type="submit" value="Edit this book">
     </form>
   </div>
@@ -19,26 +21,33 @@ export default {
   data: function() {
     return {
       message: "Welcome to Edit page!",
-      bookName: "",
-      bookPages: ""
+      book: {
+        name: "",
+        pages:""
+      },
+      errors: []
     };
   },
   created: function() {
     // get the correct data from the api
     axios.get('/api/books/' + this.$route.params.id).then(response => {
-      this.bookName = response.data.name;
-      this.bookPages = response.data.pages;
+      this.book.name = response.data.name;
+      this.book.pages = response.data.pages;
       console.log(response.data);
     })
   },
   methods: {
     submit: function() {
       var params = {
-        name: this.bookName,
-        pages: this.bookPages
+        name: this.book.name,
+        pages: this.book.pages
       }
       axios.patch('/api/books/' + this.$route.params.id, params).then(response => {
         this.$router.push('/');
+      }).catch(error => {
+        console.log('in the .catch')
+        console.log(error.response);
+        this.errors = error.response.data.errors;
       })
     }
   }
